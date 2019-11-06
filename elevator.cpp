d3c7279f293c010807eddbf2bab0cf3e8a5c08f0
sadiao <961853266@qq.com>
2019-11-07 00:13:58 +0800

    in this version setmap function will override peoplesim,this problem remain unsloved unluckily due to the restiction of time

--- elevator.cpp
+++ elevator.cpp
@@ -2,16 +2,20 @@
 #include<stdlib.h> 
 #include<math.h>
 #include<time.h>
+#include<windows.h>
+
 #define downwards 0
 #define upwards 1
-
-#define wall 1 
-#define pass 2 
-#define ele  3 
-#define person 4 
-#define max_floor 20
+#define open 2
+#define close 3
+#define wall 4
+#define pass 5 
+#define ele  6 
+#define person 7 
+#define max_floor 20      //最多有几层楼
 #define map_w 100
 #define map_h 100
+#define sleeptime 500     //运行一次间隔时间ms
 int Map[map_w][map_h];    //存放每个格子需打印的类型，wall，pass,etc. 
 int set_flag=0;           //判断是否进行过自定义设置 
 int clk=0;                //电梯运行计时器 
@@ -26,7 +30,6 @@ typedef struct floor{
 	int height;     	//楼层高度 
 	int capacity;   	//楼层最多容纳的人数 
 	int num;        	//楼层数量 
-	int current_floor;  //当前楼层 
 	List L[max_floor];             //线性表数组 
 	
 }floor_info,*ptr_f;   
@@ -35,7 +38,9 @@ typedef struct elevator{
 	int capacity;   	//电梯最多容纳人数 
 	int direction;  	//运行的方向  downwards或upwards 
 	int current_pos;	//当前纵坐标
-	int current_num;  //当前电梯人数 
+	int current_num;    //当前电梯人数 
+	int current_floor;  //当前电梯在第几层
+	int door_status;    //判断电梯门有没有开  open和close
 	int* elem;          //引入线性表存储电梯中的人的信息，表中的值代表想去的楼层数 
 }ele_info,*ptr_e;
 
@@ -46,12 +51,55 @@ void PrintBuild();
 void PrintCube(int num);
 void PrintSpace(int num);
 void PrintStar(int num);
-void set();
+void Set();
 void MainMenu();
 void DefaultSim();
+void SetMap(); 
+int GetFloor(int i);
+void ElePosChange();
+void PeopleSim();
 int RandNum(int min,int max){           //生成min<=x<=max的随机数
     int result = rand()%(max-min+1)+min;
 	return result;
+}
+void EleRun(){
+	while(1){
+		SetMap();
+		if(clk==0) PeopleSim(); 
+		if(clk==0) e->direction=upwards;
+		e->current_floor=GetFloor(e->current_pos);
+		
+		if(e->current_pos%f->height==0)
+		{  				//达到某一层
+			e->door_status=open;
+			/*
+			PeopleChange();  //人员进出
+			*/ 
+		}
+		PrintBuild();
+		clk++;
+		ElePosChange();//根据运行方向改变电梯位置
+	}
+	return;
+}
+/*
+void PeopleChange(){
+
+
+
+
+
+
+
+
+
+
+	return;
+}
+*/
+int GetFloor(int i){              //将给定纵坐标转化成楼层  最底层为0层
+	int floor=f->num-((i-1)/f->height)-1;
+	return floor;
 }
 void PeopleSim(){
 	for(int i=0;i<f->num;i++)
@@ -63,14 +111,13 @@ void PeopleSim(){
 			while((f->L[i].elem[j]=RandNum(0,f->num-1))==i){    //如果想要去的是当前楼层，则重新生成 
 				;
 			} 
-			Map[(f->num-i)*f->height-1][e->capacity+2+j]=person;          //在图上标式这是个活人
+			Map[(f->num-i)*f->height-1][e->capacity+2+j]=person;          	//在图上标识这是个活人
 			Map[(f->num-i)*f->height-2][e->capacity+2+j]=-(f->L[i].elem[j]); //这个人上方的位置 用负数的绝对值显示想去的楼层
 		}
 	}
 	return;
 }
 void PrintBuild(){
-	PeopleSim();
 	int width=3+f->capacity+e->capacity;
 	int height=f->height*f->num+2;
 	if(width>=map_w||height>=map_h)
@@ -94,6 +141,10 @@ void PrintBuild(){
 		printf("%d",i);
 		printf("\n");  
 	}
+	printf("当前已运行%d秒...\n",clk);
+	printf("电梯正在第%d层\n",e->current_floor);
+	Sleep(500);
+	system("cls");
 } 
 void SetMap(){
 	int i,j;
@@ -115,6 +166,10 @@ void SetMap(){
 			{
 				Map[i][j]=ele;
 			}		
+			if((GetFloor(i)==e->current_floor)&&(i%f->height!=0)&&(e->door_status==open)&&(j==e->capacity+1))     //控制电梯门的开关
+			{
+				Map[i][j]=pass;
+			}
 		}
 	}
 	return;
@@ -146,16 +201,16 @@ void MainMenu(){
 	scanf("%d",&choice);
 	switch(choice){
 		case(1):DefaultSim();break;   //默认设置 
-		case(2):set();break;
+		case(2):Set();break;
 		case(3):break;
 		case(4):break;
 		case(0):break;
-	}
-		
+	}	
 	return;
 }
 
-void ElePosChange(){
+void ElePosChange(){    //根据运行 方向改变电梯位置
+
 	if(e->direction==downwards)
 	{
 		e->current_pos++;	
@@ -163,6 +218,10 @@ void ElePosChange(){
 	else
 	{
 		e->current_pos--;
+	}
+	if(e->current_pos==f->height*f->num&&e->direction==downwards||e->current_pos==f->height&&e->direction==upwards)
+	{
+		e->direction=!(e->direction);
 	}
 	return;
 }
@@ -178,13 +237,16 @@ void DefaultSim(){
 	f->capacity=6;
 	e->current_pos=f->num*f->height;  //默认设置 
 	}
+	EleRun();
+	/*
 	SetMap();
-	PrintBuild();
+	PeopleSim();          //随机生成等候人的楼层分布
+	PrintBuild();*/
 	system("pause");
 	MainMenu();
 	return;
 } 
-void set(){
+void Set(){
 	int choice;
 	system("cls");
 	PrintStar(7);printf("设置");PrintStar(7);
