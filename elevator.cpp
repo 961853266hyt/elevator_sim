58ee6e2e5f7768037b09e8bb9fbae2fde596995c
sadiao <961853266@qq.com>
2019-11-06 16:56:40 +0800

    use 2d matrix to print the building

--- elevator.cpp
+++ elevator.cpp
@@ -1,8 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h> 
+#include<math.h>
 #define downwards 0
 #define upwards 1
- 
+
+#define wall 0
+#define pass 1
+#define ele  2
+#define person 3
+#define map_w 300
+#define map_h 300
+int Map[map_w][map_h];    //存放每个格子需打印的类型，wall，pass,etc. 
+int set_flag=0;            //判断是否进行过自定义设置 
 typedef struct floor{
 	int height;     	//楼层高度 
 	int capacity;   	//楼层最多容纳的人数 
@@ -18,12 +27,62 @@ typedef struct elevator{
 	
 }ele_info,*ptr_e;
 
+void PrintBuild(ptr_f f,ptr_e e);
 void PrintCube(int num);
 void PrintSpace(int num);
 void PrintStar(int num);
-void PrintBuilding(ptr_f f,ptr_e e);
 void set(ptr_f f,ptr_e e);
 void MainMenu(ptr_f f,ptr_e e);
+void default_sim(ptr_f f,ptr_e e);
+void PrintBuild(ptr_f f,ptr_e e){
+	int width=3+f->capacity+e->capacity;
+	int height=f->height*f->num+2;
+	if(width>=map_w||height>=map_h)
+	{
+		printf("输入超过限制！！！\n"); 
+		exit(0); 
+	}
+	for(int i=0;i<height;i++)
+	{
+		for(int j=0;j<width;j++)
+		{
+			switch(Map[i][j])
+			{
+				case(wall):printf("■");break;
+				case(pass):printf("  ");break;
+				case(ele): printf("**");break;
+				case(person):printf("★");break;
+				default:printf("%d",abs(Map[i][j]));break;   //用负数绝对值代表每个人要去的楼层数 
+			}                  
+		}
+		printf("%d",i);
+		printf("\n");  
+	}
+} 
+void SetMap(ptr_f f,ptr_e e){
+	int i,j;
+	int width=3+f->capacity+e->capacity;
+	for(i=0;i<=f->num*f->height+1;i++)
+	{	 
+		for(j=0;j<width;j++)
+		{	
+		
+			if(i==0||i==f->num*f->height+1||j==0||j==e->capacity+1||j==width-1||(i%f->height==0&&j>e->capacity))
+			{
+				Map[i][j]=wall;	
+			}
+			else
+			{
+				Map[i][j]=pass;
+			}
+			if(i==e->current_pos&&j>0&&j<=e->capacity)   //电梯当前位置 
+			{
+				Map[i][j]=ele;
+			}		
+		}
+	}
+	return;
+}
 
 void PrintCube(int num){           //打印num个方块 
 	for(int i=0;i<num;i++) 
@@ -39,49 +98,18 @@ void PrintStar(int num){
 	for(int i=0;i<num;i++) 
 		printf("**"); 
 }
-void PrintBuilding(ptr_f f,ptr_e e){
-	int i,j;
-	int width=3+f->capacity+e->capacity;
-	
-	PrintCube(width);    //打印顶层
-	printf("\n"); 
-	
-	for(i=1;i<=f->num*f->height;i++)
-	{
-		printf("■");
-		if(i==e->current_pos)           //判断电梯所在的位置 
-			PrintStar(e->capacity);
-		else 
-			PrintSpace(e->capacity);		
-				
-		printf("■");
-		if(i%f->height==0)
-		{
-			PrintCube(f->capacity+1);	
-		}
-		else
-		{
-			PrintSpace(f->capacity);
-			printf("■");
-		}
-		printf("\n");
-		
-	}	
-	PrintCube(width);    //打印底层 
-	printf("\n");
-	return;
-}
 
 void MainMenu(ptr_f f,ptr_e e){
+	system("cls");
 	int choice;
 	PrintStar(4);printf("电梯模拟");PrintStar(4);
 	printf("\n");
-	printf("       1、\n       2、设置\n       3、\n       4、\n       0、退出\n");
+	printf("       1、开始模拟\n       2、自定义设置\n       3、\n       4、\n       0、退出\n");
 	PrintStar(12);
 	printf("    请选择：");
 	scanf("%d",&choice);
 	switch(choice){
-		case(1):break;
+		case(1):default_sim(f,e);break;   //默认设置 
 		case(2):set(f,e);break;
 		case(3):break;
 		case(4):break;
@@ -90,7 +118,20 @@ void MainMenu(ptr_f f,ptr_e e){
 		
 	return;
 }
-
+void default_sim(ptr_f f,ptr_e e){
+	if(!set_flag)
+	{
+	printf("您还未进行设置，以下为默认设置的模拟\n"); 
+	e->capacity=7;
+	f->height=5;
+	f->num=5;
+	f->capacity=6;
+	e->current_pos=f->num*f->height;  //默认设置 
+	}
+	SetMap(f,e);
+	PrintBuild(f,e);
+	return;
+} 
 void set(ptr_f f,ptr_e e){
 	int choice;
 	system("cls");
@@ -104,15 +145,14 @@ void set(ptr_f f,ptr_e e){
 	scanf("%d",&(f->capacity));
 	printf("请输入电梯的最大容纳人数：");
 	scanf("%d",&(e->capacity));
+	e->current_pos=f->num*f->height;   //默认电梯从底层出发 
 	PrintStar(16);
+	SetMap(f,e);
 	printf("\n"); 
 	printf("设置成功！！！\n"); 
-	/*
-	f->capacity=6;
-	f->height=5;
-	f->num=4;
-	e->capacity=8;*/
-	e->current_pos=f->num*f->height;    //默认电梯底层出发 
+	set_flag=1;
+	system("pause");
+	MainMenu(f,e);
 	return;
 }
 
@@ -120,6 +160,5 @@ int main(){
 	ptr_f f=(ptr_f)malloc(sizeof(struct floor));
 	ptr_e e=(ptr_e)malloc(sizeof(struct elevator));
 	MainMenu(f,e);	
-	PrintBuilding(f,e);
 	return 0;
 }
